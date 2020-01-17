bcae74db2b9b81f178ed7fab101ab2ad035f5c34
AlvaroCarretero <59997581+AlvaroCarretero@users.noreply.github.com>
2020-01-17 12:18:34 +0100

    Bastante Useless la wea

---
+++ HolaMundo/src/HolaMundo/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package HolaMundo;
+
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Me estoy rallando la cabesa");
+        System.out.println("Somo el equipo risqueto");
+        System.out.println("Aqui trabajo sosiale");
+        System.out.println("Por la falta de respeto");
+        System.out.println("A polisia locale");
+        System.out.println("Etamo podando un abeto");
+        System.out.println("Etamo tranquilo, shurmano");
+    }
+
+}
